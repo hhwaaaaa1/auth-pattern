e3b29605a35a6b60ebeecc2794d8cd41a21a202c
hhwaaaaa1 <hhwaaaaa1@naver.com>
2023-04-20 14:45:21 +0900

    Initialize styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,20 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import { Global, css } from '@emotion/react';
+import emotionReset from 'emotion-reset';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Global
+        styles={css`
+          ${emotionReset}
+        `}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
