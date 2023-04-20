42ea0ab2f7e8dcebb39ec32fb418e89d04549a59
hhwaaaaa1 <hhwaaaaa1@naver.com>
2023-04-20 15:31:28 +0900

    Implement login UI

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import type { AppProps } from 'next/app';
 import { Global, css } from '@emotion/react';
 import emotionReset from 'emotion-reset';
+import Layout from '@/components/Layout';
+
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
@@ -8,13 +10,13 @@ import '@fontsource/roboto/700.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <Layout>
       <Global
         styles={css`
           ${emotionReset}
         `}
       />
       <Component {...pageProps} />
-    </>
+    </Layout>
   );
 }
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,3 +1,22 @@
+import Stack from '@mui/material/Stack';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+
 export default function Login() {
-  return <div>login page</div>;
+  return (
+    <Stack
+      component="form"
+      spacing={1}
+      css={{
+        width: '300px',
+        margin: 'auto',
+      }}
+    >
+      <TextField label="email" variant="outlined" />
+      <TextField label="password" variant="outlined" type="password" />
+      <Button type="submit" variant="contained">
+        SUBMIT
+      </Button>
+    </Stack>
+  );
 }
