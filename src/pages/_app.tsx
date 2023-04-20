2e9ec7f7c4cb413b62d6558f3bce39511887ee68
hhwaaaaa1 <hhwaaaaa1@naver.com>
2023-04-20 17:51:42 +0900

    Add `recoil` & debugger

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,10 @@
 import type { AppProps } from 'next/app';
 import { Global, css } from '@emotion/react';
 import emotionReset from 'emotion-reset';
+import { RecoilRoot } from 'recoil';
+
 import Layout from '@/components/Layout';
+import { DebugObserver } from '@/components/DebugObserver';
 
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
@@ -10,13 +13,16 @@ import '@fontsource/roboto/700.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Global
-        styles={css`
-          ${emotionReset}
-        `}
-      />
-      <Component {...pageProps} />
-    </Layout>
+    <RecoilRoot>
+      <DebugObserver />
+      <Layout>
+        <Global
+          styles={css`
+            ${emotionReset}
+          `}
+        />
+        <Component {...pageProps} />
+      </Layout>
+    </RecoilRoot>
   );
 }
