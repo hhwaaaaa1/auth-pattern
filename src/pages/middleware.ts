fd12328e2a1b31c9e6be1c0a5c63c98dea914cee
hhwaaaaa1 <hhwaaaaa1@naver.com>
2023-04-21 11:09:59 +0900

    feat: Authentication middleware

---
+++ src/pages/middleware.ts
@@ -0,0 +1,23 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+import jwt from 'jsonwebtoken';
+
+export function middleware(request: NextRequest) {
+  const token = request.headers
+    .get('authorization')
+    ?.split('Bearer ')[1];
+
+  if (!token) {
+    return new NextResponse('Unauthorized', { status: 401 });
+  }
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET_KEY, (error) => {
+    if (error) {
+      return new NextResponse('Unauthorized', { status: 401 });
+    }
+  });
+}
+
+export const config = {
+  matcher: '/profiles/*',
+};
